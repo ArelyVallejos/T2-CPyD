1b4cb684ab8a81c64329a1564b736c16d180f6dc
ArelyVallejos <116681940+ArelyVallejos@users.noreply.github.com>
2023-06-03 05:01:29 -0400

    Update par.cpp

--- par.cpp
+++ par.cpp
@@ -36,7 +36,7 @@ void calcularInflacion(std::vector<Mes>& totales) {
 int main() {
   std::ifstream inputfile("supermercado.csv", std::ifstream::binary);
 
-  // Lee la primera linea
+  // Ignora, salta la primera linea
   std::string line;
   std::getline(inputfile, line);
 
